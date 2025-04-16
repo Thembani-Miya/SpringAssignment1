4c3cf4bf98c31f2ed52472d8c82b85441da6fb52
Thembani-Miya <georgetuesday195@gmail.com>
2025-04-16 18:08:37 +0200

    init commit

--- src/main/java/com/example/springassignment1/DataLoader.java
+++ src/main/java/com/example/springassignment1/DataLoader.java
@@ -16,20 +16,60 @@ public class DataLoader {
     @PostConstruct
     public void init() {
         Course c1 = new Course();
-        c1.setName("Introduction to Programming");
+        c1.setName("Introduction to Programming Concepts");
         c1.setLevel("Foundation");
 
         Course c2 = new Course();
-        c2.setName("Data Structures");
-        c2.setLevel("Undergraduate");
+        c2.setName("Computer Literacy for Science");
+        c2.setLevel("Foundation");
 
         Course c3 = new Course();
-        c3.setName("Machine Learning");
-        c3.setLevel("Honours");
+        c3.setName("Data Structures and Algorithms");
+        c3.setLevel("Undergraduate");
+
+        Course c4 = new Course();
+        c4.setName("Operating Systems");
+        c4.setLevel("Undergraduate");
+
+        Course c5 = new Course();
+        c5.setName("Database Management and Design");
+        c5.setLevel("Undergraduate");
+
+        Course c6 = new Course();
+        c6.setName("Introduction to Computer Networks");
+        c6.setLevel("Undergraduate");
+
+        Course c7 = new Course();
+        c7.setName("Software Engineering");
+        c7.setLevel("Undergraduate");
+
+        Course c8 = new Course();
+        c8.setName("Advanced Java");
+        c8.setLevel("Honours");
+
+        Course c9 = new Course();
+        c9.setName("Intelligent Systems");
+        c9.setLevel("Honors");
+
+        Course c10 = new Course();
+        c10.setName("Theory of Computing");
+        c10.setLevel("Honors");
+
+        Course c11 = new Course();
+        c11.setName("Computer Graphics");
+        c11.setLevel("Honors");
 
         controller.addCourse(c1);
         controller.addCourse(c2);
         controller.addCourse(c3);
+        controller.addCourse(c4);
+        controller.addCourse(c5);
+        controller.addCourse(c6);
+        controller.addCourse(c7);
+        controller.addCourse(c8);
+        controller.addCourse(c9);
+        controller.addCourse(c10);
+        controller.addCourse(c11);
 
         System.out.println("📦 Sample courses loaded!");
     }
